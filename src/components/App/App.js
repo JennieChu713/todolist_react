255ae13c11a341b4e893d8171203c9d3d8f2ebcb
JennieChu713 <38804711+JennieChu713@users.noreply.github.com>
2021-10-04 14:07:47 +0800

    completed: tidy-up usecontext components.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,49 +1,16 @@
-import React, { useContext, createContext } from "react";
-// import { PropTypes } from "prop-types";
+import React from "react";
 import TodosForm from "../Todos/TodosForm";
 import TodoStatus from "../Todos/TodosList/TodosStatus";
 import TodoItem from "../Todos/TodosList/TodoItem";
+import TodoFilters from "../Todos/TodoFilters/TodoFilters";
 import useTodos from "../../useTodos";
-import filterButtonsComponents from "../Todos/FilterButtonsComponents";
 import generalComponents from "../GeneralComponents";
+import { FilterBtnsContext } from "../../FilterBtnsContext";
 
 // CSS in JS
 // general components
 const { Body, TodoListContainer, TodoListTitle, TodoListout } =
   generalComponents();
-
-// status filter buttons
-const {
-  TodoListFilterBtns,
-  UndoneBtn,
-  DoneBtn,
-  AllBtn,
-  ClearDoneBtn,
-  ClearAllBtn,
-} = filterButtonsComponents();
-
-// status filter buttons structure
-const TodoFilterGroup = () => {
-  const {
-    handleUndoneTodos,
-    handleDoneTodos,
-    handleAllTodos,
-    handleClearAllTodos,
-    handleClearDoneTodos,
-  } = useContext(FilterBtnsContext);
-  return (
-    <TodoListFilterBtns>
-      <UndoneBtn onClick={handleUndoneTodos}>Undone</UndoneBtn>
-      <DoneBtn onClick={handleDoneTodos}>Done</DoneBtn>
-      <AllBtn onClick={handleAllTodos}>All</AllBtn>
-      <ClearDoneBtn onClick={handleClearDoneTodos}>Clear Done</ClearDoneBtn>
-      <ClearAllBtn onClick={handleClearAllTodos}>Clear All</ClearAllBtn>
-    </TodoListFilterBtns>
-  );
-};
-
-// useContext for filter buttons
-const FilterBtnsContext = createContext();
 
 // front render structure and render logics
 function App() {
@@ -84,7 +51,7 @@ function App() {
               handleClearAllTodos,
             }}
           >
-            <TodoFilterGroup />
+            <TodoFilters />
           </FilterBtnsContext.Provider>
           <TodoStatus
             total={todos.length}
